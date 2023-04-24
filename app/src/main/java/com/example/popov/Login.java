3e867e181b4b22d7ee7c87904d53c42713d707e5
Antarktida <93553146+qwingel@users.noreply.github.com>
2023-04-25 01:33:56 +0300

    Update Login.java

--- app/src/main/java/com/example/popov/Login.java
+++ app/src/main/java/com/example/popov/Login.java
@@ -28,7 +28,7 @@ public class Login extends AppCompatActivity {
     String session, Access;
 
 
-    public static final String SQurl = "http://192.168.43.40";
+    public static final String SQurl = "urll";
 
     public static class ResponseMessage {
         public String status, message;
